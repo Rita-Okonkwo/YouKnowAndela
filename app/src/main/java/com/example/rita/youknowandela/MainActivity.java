0093a0f44b21220a1430c4bc5f35d9034960a07c
Rita-Okonkwo <ritaokonkwo6@gmail.com>
2018-06-30 13:24:18 +0100

    first commit

---
+++ app/src/main/java/com/example/rita/youknowandela/MainActivity.java
@@ -0,0 +1,85 @@
+package com.example.rita.youknowandela;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    boolean answerFirst;
+    boolean answerSecond;
+    boolean answerThird;
+    int totalScore = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getNameOfGenius();
+    }
+
+    public void doneButton(View view) {
+        int score = getTotalScore();
+        displayMessage(score);
+
+        Context context = getApplicationContext();
+        CharSequence text = displayMessage(score);
+        int duration = Toast.LENGTH_LONG;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+
+    }
+    //Method to get the name of user
+    //Method names begin usually with verbs
+
+    public Editable getNameOfGenius() {
+        EditText nameOfPerson = (EditText) findViewById(R.id.name);
+        Editable name = nameOfPerson.getText();
+
+        return name;
+    }
+    public void radioButton(View view) {
+        RadioButton firstAnswer = (RadioButton) findViewById(R.id.brilliance);
+        answerFirst = firstAnswer.isChecked();
+
+    }
+
+    public void radioButton1(View view) {
+        RadioButton secondAnswer = (RadioButton) findViewById(R.id.may);
+        answerSecond = secondAnswer.isChecked();
+    }
+
+    public void radioButton2(View view) {
+        RadioButton thirdAnswer = (RadioButton)  findViewById(R.id.newyork);
+        answerThird = thirdAnswer.isChecked();
+    }
+
+    public int getTotalScore() {
+        if(answerFirst){
+            totalScore = 10;
+        }
+        else{
+            totalScore = 0;
+        }
+        if(answerSecond) {
+            totalScore =  10;
+        }
+        else{
+            totalScore = 0;
+        }
+        return totalScore;
+    }
+
+
+    public String displayMessage(int totalScore){
+         String message = "The total score is " + totalScore;
+
+        return message;
+    }
+}
